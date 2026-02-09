ea099d9df88b5566d0a572964f2f6700d9dad7c1
Nguyen Hoang Long <bangbanginmeblog@gmail.com>
2026-02-09 22:57:51 +0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using AddinVeMong.Views;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+
+namespace AddinVeMong
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            GiaoDien window = new GiaoDien();
+            window.ShowDialog();
+
+            return Result.Succeeded;
+        }
+    }
+}
