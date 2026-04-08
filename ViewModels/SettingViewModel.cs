6c4d4047aca0645f6bb95d7b97e0e43d5a095d90
Nguyen Hoang Long <bangbanginmeblog@gmail.com>
2026-04-08 16:56:27 +0700

    Lập trình View Cài đặt

---
+++ ViewModels/SettingViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AddinVeMong.ViewModels
+{
+    public class SettingViewModel
+    {
+        public string TitleText { get; set; }
+
+        public SettingViewModel()
+        {
+            // Nội dung hiển thị cho Label trong view Cài đặt
+            TitleText = "Cấu hình Add-in Thiết kế móng";
+        }
+    }
+}
