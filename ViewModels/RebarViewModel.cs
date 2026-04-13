7a8c6bca1eb5402240e6bc2f2f978d0f055e9296
Nguyen Hoang Long <bangbanginmeblog@gmail.com>
2026-04-13 10:37:25 +0700

    Xóa phần thừa file RebarViewModel.cs

--- ViewModels/RebarViewModel.cs
+++ ViewModels/RebarViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Autodesk.Revit.UI;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using AddinVeMong.Commands;
@@ -8,43 +7,27 @@ namespace AddinVeMong.ViewModels
 {
     public class RebarViewModel : INotifyPropertyChanged
     {
-        private ExternalCommandData _commandData;
-
-        // Thép phương dài
+        // Dữ liệu chỉ để Binding với UI
         public int LongCount { get; set; } = 8;
         public int LongDiameter { get; set; } = 14;
-
-        // Thép phương ngắn
         public int ShortCount { get; set; } = 6;
         public int ShortDiameter { get; set; } = 14;
-
-        // Thép chờ cột
         public int StarterCount { get; set; } = 4;
         public int StarterDiameter { get; set; } = 18;
 
         public ICommand DrawRebarCommand { get; }
 
-        public RebarViewModel(ExternalCommandData commandData)
+        public RebarViewModel()
         {
-            _commandData = commandData;
+            // Command bây giờ sẽ gọi một lớp xử lý riêng
             DrawRebarCommand = new RelayCommand(ExecuteDraw);
         }
 
         private void ExecuteDraw(object? obj)
         {
-            // Lấy đối tượng đang chọn trong Revit
-            var uiDoc = _commandData.Application.ActiveUIDocument;
-            var selectedIds = uiDoc.Selection.GetElementIds();
-
-            if (selectedIds.Count == 0)
-            {
-                TaskDialog.Show("Lỗi", "Vui lòng chọn ít nhất 1 cái móng trước khi bấm nút!");
-                return;
-            }
-
-            // Gọi Transaction để vẽ thép (Phần này ta sẽ viết tiếp)
-
-            TaskDialog.Show("Revit", $"Đang vẽ cho {selectedIds.Count} móng...");
+            // Chỉ cần viết
+            // RebarLogic.Run(this); 
+            // (Trong đó RebarLogic là file trong thư mục Commands)
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
