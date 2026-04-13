7eed393305a2f034890c48e5dd5b90f7e273319d
Nguyen Hoang Long <bangbanginmeblog@gmail.com>
2026-04-13 14:52:47 +0700

    Cập nhật giao diện form About

--- ViewModels/AboutViewModel.cs
+++ ViewModels/AboutViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace AddinVeMong.ViewModels
+﻿using System;
+using System.Reflection;
+
+namespace AddinVeMong.ViewModels
 {
     public class AboutViewModel
     {
@@ -6,7 +9,20 @@
 
         public AboutViewModel()
         {
-            DisplayText = "Giới thiệu Addin";
+            string version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "1.0.0";
+            string userName = Environment.UserName;
+            string computerName = Environment.MachineName;
+
+            DisplayText = $"PROJECT: ADD-IN THIẾT KẾ MÓNG ĐƠN VÁT\n" +
+              $"Phiên bản: {version} (Beta)\n" +
+              $"Sản phẩm thuộc đồ án môn học: Lập trình ứng dụng trong xây dựng\n\n" +
+              $"Thực hiện bởi: Nhóm\n" +
+              $"- Sinh viên 1: Nguyễn Hoàng Long\n" +
+              $"- Sinh viên 2: Đoàn Quang Ánh\n" +
+              $"- Sinh viên 3: Nguyễn Hữu Hùng\n" +
+              $"Giảng viên hướng dẫn: Thầy Nguyễn Văn Hải\n" +
+              $"------------------------------------------\n" +
+              $"Cảm ơn thầy/cô đã sử dụng Add-in trên thiết bị: {computerName}";
         }
     }
 }
